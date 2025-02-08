238f1df0f0bb5222e60c761e69e05fc418949d9d
junsoo1003 <royalex@dgu.ac.kr>
2025-02-08 21:44:33 +0900

    [T1-433] feat: bin entity 구현

--- src/main/java/com/example/wms/order/application/domain/Supplier.java
+++ src/main/java/com/example/wms/order/application/domain/Supplier.java
@@ -7,11 +7,11 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class Supplier {
-    private Long supplierId;
-    private String supplierName;
-    private String businessNumber;
-    private String representativeName;
-    private String address;
-    private String supplierPhone;
-    private String managerPhone;
+    private Long supplierId; // 공급자 고유 ID
+    private String supplierName; // 공급자 이름
+    private String businessNumber; // 사업자 등록 번호
+    private String representativeName; // 대표자 이름
+    private String address; // 공급자 주소
+    private String supplierPhone; // 공급자 전화번호
+    private String managerPhone; // 공급자 관리자 전화번호
 }
--- src/main/java/com/example/wms/product/application/domain/Product.java
+++ src/main/java/com/example/wms/product/application/domain/Product.java
@@ -8,16 +8,16 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class Product {
-    private Long productId;
-    private String productCode;
-    private String productName;
-    private Integer purchasePrice;
-    private Integer salePrice;
-    private Integer lotUnit;
-    private Long supplierId;
-    private Integer stockLotCount;
-    private String category;
-    private String minLotCount;
-    private Integer leadTime;
+    private Long productId; // 제품 고유 ID
+    private String productCode; // 제품 코드
+    private String productName; // 제품 이름
+    private Integer purchasePrice; // 구매 가격
+    private Integer salePrice; // 판매 가격
+    private Integer lotUnit; // 로트당 제품 수량
+    private Long supplierId; // 공급자 ID
+    private Integer stockLotCount; // 재고 로트 수량
+    private String category; // 카테고리
+    private String minLotCount; // 최소 LOT 수량
+    private Integer leadTime; // 이 품목이 납품업체로부터 납품될 때 까지 걸리는 시간
     private String locationBinCode;
 }
