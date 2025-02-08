4b1e49620e527849865e6c7c0a3ec21aa28f6a31
N-jongsik <jongsik0625@naver.com>
2025-02-08 17:03:02 +0900

    feat: T1-434 <outbound entity 구현>

--- src/main/java/com/example/wms/notification/application/domain/Notification.java
+++ src/main/java/com/example/wms/notification/application/domain/Notification.java
@@ -5,15 +5,16 @@ import com.example.wms.user.application.domain.enums.UserRole;
 import lombok.*;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Notification {
-    private Long notificationId;
+    private Long notificationId; // 알림 ID
 
-    private String content;
+    private String content; // 채팅 내용
 
     private Integer event;
+    // 1.출고예정, 2.발주신청, 3.발주승인, 4.입하심사, 5.입고적치, 6.출고지시, 7.출고피킹, 8.출고패킹, 9.출하상차
 
-    private UserRole userRole;
+    private UserRole userRole; // 사용자 유형
 }
---
+++ src/main/java/com/example/wms/outbound/application/domain/OutboundPlanProduct.java
@@ -0,0 +1,4 @@
+package com.example.wms.outbound.application.domain;
+
+public class OutboundPlanProduct {
+}
