95bfc4f9c22f09a08c8f31bf3af48e73b89aad22
N-jongsik <jongsik0625@naver.com>
2025-02-08 17:08:31 +0900

    feat: T1-439 <outboundPlanProduct entity 구현>

--- src/main/java/com/example/wms/outbound/application/domain/OutboundPlanProduct.java
+++ src/main/java/com/example/wms/outbound/application/domain/OutboundPlanProduct.java
@@ -1,4 +1,26 @@
 package com.example.wms.outbound.application.domain;
 
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class OutboundPlanProduct {
+    private Long outboundPlanProductId;
+
+    private Long outboundPlanId;
+
+    private Long productId;
+
+    private Integer requiredQuantity;
+
+    private Integer StockUsedQuantity;
+
+    private Integer OrderQuantity;
+
+    private String status;
 }
