c6fcdff390d458cfd0b5bb58a0b06e8f5a668669
shining013 <nevertheless113@naver.com>
2025-02-10 09:49:11 +0900

    fix: BaseEntity 수정

--- src/main/java/com/example/wms/infrastructure/entity/BaseEntity.java
+++ src/main/java/com/example/wms/infrastructure/entity/BaseEntity.java
@@ -1,24 +1,28 @@
 package com.example.wms.infrastructure.entity;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
-@EntityListeners(AuditingEntityListener.class)
-@MappedSuperclass
+@Setter
 public abstract class BaseEntity {
 
-    @CreatedDate
-    @Column(updatable = false)
-    private LocalDate createdAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
-    @LastModifiedDate
-    private LocalDate updatedAt;
+    // 삽입 전
+    public void preInsert() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    // 업데이트 시
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 }
