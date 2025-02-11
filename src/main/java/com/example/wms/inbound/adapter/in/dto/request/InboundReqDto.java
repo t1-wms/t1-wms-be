d7ab66b96039768a076fdb210b46dbc3eec5fdc2
shining013 <nevertheless113@naver.com>
2025-02-11 21:06:58 +0900

    입고 예정 dto 생성

---
+++ src/main/java/com/example/wms/inbound/adapter/in/dto/request/InboundReqDto.java
@@ -0,0 +1,22 @@
+package com.example.wms.inbound.adapter.in.dto.request;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class InboundReqDto {
+    private LocalDate scheduleDate; // order의 delivery_deadLine 값을 저장하는 것임
+    private Long orderId; // 주문 id
+    private Long supplierId; // 공급처 번호
+    private String orderNumber; // 주문 번호
+    private LocalDateTime orderDate; // 주문 날짜
+}
