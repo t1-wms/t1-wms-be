4063962a28432a9539a629c18bb82e9b2b8c2f06
shining013 <nevertheless113@naver.com>
2025-02-12 12:07:19 +0900

    chore: 테스트용 order 관련 구현체 삭제

--- src/main/java/com/example/wms/inbound/adapter/in/InboundController.java
+++ src/main/java/com/example/wms/inbound/adapter/in/InboundController.java
@@ -2,15 +2,11 @@ package com.example.wms.inbound.adapter.in;
 
 import com.example.wms.inbound.adapter.in.dto.request.InboundReqDto;
 import com.example.wms.inbound.application.port.in.InboundUseCase;
-import com.example.wms.order.adapter.in.dto.response.OrderApproveResDto;
-import com.example.wms.order.application.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,7 +14,6 @@ import java.util.List;
 public class InboundController {
 
     private final InboundUseCase inboundUseCase;
-    private final OrderService orderService;
 
     public ResponseEntity<Void> createInbound(@RequestBody InboundReqDto inboundReqDto) {
         inboundUseCase.createInboundPlan(inboundReqDto); // 수동 생성
