8eb72c23a2a3b0c2700e5148e21d0be857370680
shining013 <nevertheless113@naver.com>
2025-02-13 17:13:37 +0900

    feat: 입하 예정 조회 테스트 코드 작성

--- src/test/java/com/example/wms/inbound/application/service/InboundServiceTest.java
+++ src/test/java/com/example/wms/inbound/application/service/InboundServiceTest.java
@@ -1,9 +1,11 @@
 package com.example.wms.inbound.application.service;
 
+import com.example.wms.inbound.adapter.in.dto.response.InboundPlanProductDto;
 import com.example.wms.inbound.adapter.in.dto.response.InboundProductDto;
 import com.example.wms.inbound.adapter.in.dto.response.InboundResDto;
 import com.example.wms.inbound.application.port.out.InboundRetrievalPort;
 import com.example.wms.order.application.domain.OrderProduct;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,13 +13,20 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 @ExtendWith(MockitoExtension.class)
 class InboundServiceTest {
@@ -29,6 +38,54 @@ class InboundServiceTest {
 
     @InjectMocks
     InboundService inboundService;
+
+    private List<InboundPlanProductDto> mockInboundPlanProductList;
+
+    @BeforeEach
+    void setUp() {
+        mockInboundPlanProductList = Arrays.asList(
+                new InboundPlanProductDto(1L,"입고중",LocalDateTime.now(),"Schedule1", LocalDate.now(), 1L, "Order1",LocalDate.now(),1L,"Supplier1",1L,"P001","Product1",10,5),
+                new InboundPlanProductDto(2L,"입고완료",LocalDateTime.now(),"Schedule2", LocalDate.now(), 2L, "Order2",LocalDate.now(),2L,"Supplier2",2L,"P002","Product2",20,10)
+        );
+    }
+
+    @Test
+    void testGetInboundPlans() {
+        Pageable pageable = PageRequest.of(0,10);
+        when(inboundRetrievalPort.findInboundProductListWithPagination(any(Pageable.class)))
+                .thenReturn(mockInboundPlanProductList);
+        when(inboundRetrievalPort.countAllInboundPlan()).thenReturn(mockInboundPlanProductList.size());
+
+        Page<InboundPlanProductDto> result = inboundService.getInboundPlans(pageable);
+
+        assertThat(result).isNotNull();
+        assertThat(result.getContent()).hasSize(mockInboundPlanProductList.size());
+        assertThat(result.getTotalElements()).isEqualTo(mockInboundPlanProductList.size());
+
+        verify(inboundRetrievalPort,times(1)).findInboundProductListWithPagination(any(Pageable.class));
+        verify(inboundRetrievalPort, times(1)).countAllInboundPlan();
+    }
+
+    @Test
+    void testGetFilteredInboundPlans() {
+        String inboundScheduleNumber = "IS202502060001";
+        LocalDate startDate = LocalDate.of(2025,2,10);
+        LocalDate endDate = LocalDate.of(2025,2,13);
+
+        when(inboundRetrievalPort.findInboundFilteringWithPagination(eq(inboundScheduleNumber), eq(startDate),eq(endDate),any(Pageable.class)))
+                .thenReturn(mockInboundPlanProductList);
+        when(inboundRetrievalPort.countFilteredInboundPlan(eq(inboundScheduleNumber), eq(startDate),eq(endDate))
+        ).thenReturn(mockInboundPlanProductList.size());
+
+        Page<InboundPlanProductDto> result = inboundService.getFilteredInboundPlans(inboundScheduleNumber, startDate, endDate);
+
+        assertThat(result).isNotNull();
+        assertThat(result.getContent()).hasSize(mockInboundPlanProductList.size());
+        assertThat(result.getTotalElements()).isEqualTo(mockInboundPlanProductList.size());
+
+        verify(inboundRetrievalPort, times(1)).findInboundFilteringWithPagination(eq(inboundScheduleNumber), eq(startDate), eq(endDate), any(Pageable.class));
+        verify(inboundRetrievalPort, times(1)).countFilteredInboundPlan(eq(inboundScheduleNumber), eq(startDate), eq(endDate));
+    }
 
     @Test
     void getInboundProductListTest() {
@@ -74,4 +131,9 @@ class InboundServiceTest {
 
         inboundResDtos.forEach(dto->logger.info(dto.toString()));
     }
+
+
+
+
+
 }
