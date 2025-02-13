4f5cb708036649b99513263da5e38b2bc5692265
shining013 <nevertheless113@naver.com>
2025-02-13 18:21:27 +0900

    feat: 입하 예정 필터링/전체 조회 어댑터 작성

--- src/main/java/com/example/wms/inbound/adapter/out/InboundRetrievalAdapter.java
+++ src/main/java/com/example/wms/inbound/adapter/out/InboundRetrievalAdapter.java
@@ -1,11 +1,15 @@
 package com.example.wms.inbound.adapter.out;
 
+import com.example.wms.inbound.adapter.in.dto.response.InboundPlanProductDto;
 import com.example.wms.inbound.adapter.in.dto.response.InboundResDto;
+import com.example.wms.inbound.application.domain.Inbound;
 import com.example.wms.inbound.application.port.out.InboundRetrievalPort;
 import com.example.wms.infrastructure.mapper.InboundRetrievalMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Component
@@ -17,5 +21,25 @@ public class InboundRetrievalAdapter implements InboundRetrievalPort {
     @Override
     public List<InboundResDto> findInboundProductListByOrderId(Long orderId) {
         return inboundRetrievalMapper.findInboundProductListByOrderId(orderId);
+    }
+
+    @Override
+    public List<InboundPlanProductDto> findInboundProductListWithPagination(Pageable pageable) {
+        return inboundRetrievalMapper.findInboundProductListWithPagination(pageable);
+    }
+
+    @Override
+    public List<InboundPlanProductDto> findInboundFilteringWithPagination(String inboundScheduleNumber, LocalDate startDate, LocalDate endDate, Pageable pageable) {
+        return inboundRetrievalMapper.findInboundFilteringWithPagination(inboundScheduleNumber, startDate, endDate, pageable);
+    }
+
+    @Override
+    public Integer countAllInboundPlan() {
+        return inboundRetrievalMapper.countAllInboundPlan();
+    }
+
+    @Override
+    public Integer countFilteredInboundPlan(String inboundScheduleNumber, LocalDate startDate, LocalDate endDate) {
+        return inboundRetrievalMapper.countAllInboundPlanFiltering(inboundScheduleNumber, startDate, endDate);
     }
 }
