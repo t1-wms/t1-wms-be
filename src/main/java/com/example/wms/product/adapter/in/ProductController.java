4369d121d88a63dd8e44d830247bec6e0d46f9ac
junsoo1003 <royalex@dgu.ac.kr>
2025-02-13 10:49:03 +0900

    [T1-22] 품목조회 api (#29)
    * [T1-22] feat: 컨트롤러 작성, 서비스 로직 작성, 쿼리문 작성
    
    * [T1-22] feat: pageable default size 수정
    
    * [T1-22] feat: 테스트 코드 작성
    
    * [T1-22] fix: 변경된 타입에 맞춰서 수정
    
    * [T1-22] feat: API 문서화, fix: product의 필드 타입, 이름 변경

--- src/main/java/com/example/wms/product/adapter/in/ProductController.java
+++ src/main/java/com/example/wms/product/adapter/in/ProductController.java
@@ -2,6 +2,8 @@ package com.example.wms.product.adapter.in;
 
 import com.example.wms.product.application.domain.Product;
 import com.example.wms.product.application.port.in.ProductUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/product")
+@Tag(name = "Product", description = "Product 관련 API")
 public class ProductController {
 
     private final ProductUseCase productUseCase;
@@ -32,6 +35,10 @@ public class ProductController {
     }
 
     @GetMapping
+    @Operation(
+            summary = "전체 품목 조회",
+            description = "전체 품목 목록을 페이지별로 조회할 수 있습니다."
+    )
     public ResponseEntity<Page<Product>> findAllBinsWithDetails(@ParameterObject Pageable pageable) {
         return ResponseEntity.ok(productUseCase.getAllProducts(pageable));
     }
