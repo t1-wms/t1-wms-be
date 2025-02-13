3e1779801a155297b6f6e7d2208b89859daa7f16
shining013 <nevertheless113@naver.com>
2025-02-13 14:51:05 +0900

    feat: 입하 예정 조회 dto 작성

---
+++ src/main/java/com/example/wms/inbound/adapter/in/dto/response/InboundResDto.java
@@ -0,0 +1,28 @@
+package com.example.wms.inbound.adapter.in.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class InboundResDto {
+    private Long inboundId;
+    private String inboundStatus;
+    private LocalDateTime createdAt;
+    private String scheduleNumber;
+    private LocalDateTime scheduleDate;
+    private Long orderId;
+    private String orderNumber;
+    private LocalDateTime orderDate;
+    private Long supplierId;
+    private String supplierName;
+    private List<InboundProductDto> productList;
+}
+
