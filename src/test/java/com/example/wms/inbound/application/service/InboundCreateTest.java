d16513940470264a3ee7c6f2f6ac5d41836311b9
shining013 <nevertheless113@naver.com>
2025-02-14 14:19:59 +0900

    feat: 입하 예정 생성 테스트 구현

--- src/test/java/com/example/wms/inbound/application/service/InboundCreateTest.java
+++ src/test/java/com/example/wms/inbound/application/service/InboundCreateTest.java
@@ -1,72 +1,45 @@
 package com.example.wms.inbound.application.service;
 
-import com.example.wms.inbound.adapter.in.dto.request.InboundReqDto;
+import com.example.wms.inbound.application.domain.Inbound;
 import com.example.wms.inbound.application.port.out.AssignInboundNumberPort;
 import com.example.wms.inbound.application.port.out.InboundPort;
-import org.junit.jupiter.api.BeforeEach;
+import com.example.wms.order.application.domain.Order;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.time.LocalDate;
-import static org.mockito.Mockito.*;
+import java.time.LocalDateTime;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class InboundCreateTest {
+
+    @InjectMocks
+    private InboundService inboundService;
 
     @Mock
     private InboundPort inboundPort;
 
     @Mock
-    private AssignInboundNumberPort inboundNumberPort;
+    private AssignInboundNumberPort assignInboundNumberPort;
 
-    @Mock
-    private InboundService inboundService;
-
-
-    @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
-
-    // 승인된 주문 조회
-    // order 정보 중
-//    {
-//        delivery_deadLine: String
-//        supplierId: Long
-//        orderId: Long
-//        orderNumber: String
-//        orderDate: String 이 값들만 사용 -> dto를 사용?
-//    }
-
-    // 재고가 부족해서 wms에서 발주 신청을 하면 납품업체에서
-    // 승인하는 경우 납품기한을 보냄  (=승인날짜 + 리드 타임)
-    // 그러면 입하 예정이 생성됨 (schedule Date = 납품기한임)
-    // inbound schedule Date <- InboundDate 로 저장함
-
-     @Test
-     @DisplayName("inbound 저장 테스트")
-     void makeScheduleInbound() {
+    @Test
+    @DisplayName("입하 스케줄을 생성하면 InboundPort의 save 메서드가 호출되어야 한다.")
+    public void testCreateInboundSchedule() {
 
         // given
-        LocalDate date = LocalDate.of(2025,3,25);
-
-        InboundReqDto inboundReqDto = InboundReqDto.builder()
-                .scheduleDate(date)
+        Order order = Order.builder()
                 .orderId(1L)
+                .supplierId(1L)
+                .inboundDate(LocalDateTime.of(2025,2,14,10,0))
                 .build();
 
-        doNothing().when(inboundService).createInboundPlan(inboundReqDto);
+        inboundService.createInboundSchedule(order);
 
-        // when
-        inboundService.createInboundPlan(inboundReqDto);
-
-        // then
-        verify(inboundService, times(1)).createInboundPlan(inboundReqDto);
-     }
-
-
+        verify(inboundPort, times(1)).save(any(Inbound.class));
+    }
 }
