5f51e605508334d84e9b61c4af99e79c3f8106f1
joo9837 <joo9837@naver.com>
2025-02-14 21:57:47 +0900

    [T1-277] feat: build test

--- src/main/java/com/example/wms/user/adapter/in/AuthController.java
+++ src/main/java/com/example/wms/user/adapter/in/AuthController.java
@@ -19,7 +19,7 @@ import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-//테스트
+// test
 @Tag(name = "auth", description = "auth domain apis")
 @Slf4j
 @RestController
