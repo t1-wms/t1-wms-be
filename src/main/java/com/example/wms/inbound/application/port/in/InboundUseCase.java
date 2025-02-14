fd5873f2c4c41d3279311863e1ea5c416b3e79bf
shining013 <nevertheless113@naver.com>
2025-02-14 12:40:18 +0900

    feat: 입하 예정 생성 구현

--- src/main/java/com/example/wms/inbound/application/port/in/InboundUseCase.java
+++ src/main/java/com/example/wms/inbound/application/port/in/InboundUseCase.java
@@ -3,6 +3,7 @@ package com.example.wms.inbound.application.port.in;
 import com.example.wms.inbound.adapter.in.dto.request.InboundReqDto;
 import com.example.wms.inbound.adapter.in.dto.response.InboundPlanProductDto;
 import com.example.wms.inbound.adapter.in.dto.response.InboundResDto;
+import com.example.wms.order.application.domain.Order;
 import com.example.wms.order.application.domain.OrderProduct;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,8 +12,9 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface InboundUseCase {
-    void createInboundPlan(InboundReqDto inboundReqDto);
+    Long createInboundPlan(InboundReqDto inboundReqDto);
     Page<InboundResDto> getInboundPlans(Pageable pageable);
     Page<InboundResDto> getFilteredInboundPlans(String inboundScheduleNumber, LocalDate startDate, LocalDate endDate, Pageable pageable);
     List<InboundResDto> getAllInboundProductList(OrderProduct orderProduct);
+    void createInboundSchedule(Order order);
 }
