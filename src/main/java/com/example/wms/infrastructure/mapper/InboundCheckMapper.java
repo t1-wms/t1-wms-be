8bb03d3adaff5fcf376a0f957338dd19aa00d6b2
shining013 <nevertheless113@naver.com>
2025-02-15 14:25:38 +0900

    feat: 입하 검사 mapper 생성

---
+++ src/main/java/com/example/wms/infrastructure/mapper/InboundCheckMapper.java
@@ -0,0 +1,13 @@
+package com.example.wms.infrastructure.mapper;
+
+import com.example.wms.inbound.application.domain.InboundCheck;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface InboundCheckMapper {
+    void insertInboundCheck(InboundCheck inboundCheck);
+    List<InboundCheck> findByInboundId(@Param("inboundId") Long inboundId);
+}
