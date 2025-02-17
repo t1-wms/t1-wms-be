a1fa1213c3ad2e041f33cd43b23fd14633236445
shining013 <nevertheless113@naver.com>
2025-02-17 16:04:07 +0900

    fix: 입하 품목 조회 컨트롤러 swagger 수정

--- src/main/java/com/example/wms/inbound/adapter/in/InboundProductGetController.java
+++ src/main/java/com/example/wms/inbound/adapter/in/InboundProductGetController.java
@@ -3,6 +3,8 @@ package com.example.wms.inbound.adapter.in;
 import com.example.wms.inbound.adapter.in.dto.response.ProductInboundResDto;
 import com.example.wms.inbound.application.port.in.InboundUseCase;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
@@ -27,9 +29,15 @@ public class InboundProductGetController {
     @GetMapping
     @Operation(summary = "품목별 입고 목록 조회하기" , description = "필터링 값이 없으면 전체 조회합니다.")
     public ResponseEntity<Page<ProductInboundResDto>> getAllInboundByProduct(
+
+            @Parameter(name ="startDate", in = ParameterIn.QUERY, description = "시작 날짜")
             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+
+            @Parameter(name = "endDate", in = ParameterIn.QUERY, description = "종료 날짜")
             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate,
+
             @ParameterObject Pageable pageable) {
         return ResponseEntity.ok(inboundUseCase.getAllInboundByProductWithPagination(startDate, endDate, pageable));
     }
+
 }
