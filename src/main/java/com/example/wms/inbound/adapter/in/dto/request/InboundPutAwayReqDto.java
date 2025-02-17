66233c575e12dab1a77a5a026d84643f3a6132cb
shining013 <nevertheless113@naver.com>
2025-02-17 16:23:49 +0900

    feat: dto에 swagger추가

--- src/main/java/com/example/wms/inbound/adapter/in/dto/request/InboundPutAwayReqDto.java
+++ src/main/java/com/example/wms/inbound/adapter/in/dto/request/InboundPutAwayReqDto.java
@@ -1,5 +1,6 @@
 package com.example.wms.inbound.adapter.in.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,9 +10,15 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "입고 적치 생성 요청 dto")
 public class InboundPutAwayReqDto {
 
+    @Schema(description = "품목 id", example = "100")
     private Long productId;
+
+    @Schema(description = "품목 별 lot 단위 개수", example = "3")
     private Integer lotCount;
+
+    @Schema(description = "품목 적치 위치", example = "A-01-01-01")
     private Long binId;
 }
