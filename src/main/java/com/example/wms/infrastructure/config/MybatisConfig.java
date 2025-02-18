4bca5591d610ad224a50e5181385fb666f54bb5a
joo9837 <joo9837@naver.com>
2025-02-18 15:16:12 +0900

    [T1-277] feat: build test

--- src/main/java/com/example/wms/infrastructure/config/MybatisConfig.java
+++ src/main/java/com/example/wms/infrastructure/config/MybatisConfig.java
@@ -2,16 +2,16 @@ package com.example.wms.infrastructure.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.type.JdbcType;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 import javax.sql.DataSource;
-
 @Configuration
 @MapperScan(
         basePackages = "com.example.wms.infrastructure.mapper",
@@ -20,19 +20,26 @@ import javax.sql.DataSource;
 )
 @Slf4j
 public class MybatisConfig {
-
-    @Value("${mybatis.mapper-locations}")
-    private String mapperPath;
-
-    @Value("${mybatis.type-aliases-package}")
-    private String typeAliasesPackage;
-
     @Bean(name = "sqlSessionFactory")
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource, ApplicationContext applicationContext) throws Exception {
         SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
         factoryBean.setDataSource(dataSource);
-        factoryBean.setMapperLocations(applicationContext.getResources(mapperPath));
-        factoryBean.setTypeAliasesPackage(typeAliasesPackage);
+
+        // 매퍼 리소스 경로 설정
+        Resource[] mapperResources = applicationContext.getResources("classpath:mybatis/**/*.xml");
+        factoryBean.setMapperLocations(mapperResources);
+
+        // 타입 별칭 패키지 설정
+        factoryBean.setTypeAliasesPackage("com.example.wms.**.domain");
+
+        // MyBatis 설정
+        org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration();
+        configuration.setMapUnderscoreToCamelCase(true);
+        configuration.setDefaultStatementTimeout(30);
+        configuration.setCallSettersOnNulls(true);
+        configuration.setJdbcTypeForNull(JdbcType.VARCHAR);
+        factoryBean.setConfiguration(configuration);
+
         return factoryBean.getObject();
     }
 
