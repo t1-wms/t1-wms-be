0e74e64fe819f4bae7960692f29d77ebc17c4f1c
N-jongsik <jongsik0625@naver.com>
2025-02-18 16:15:03 +0900

    [T1-57] feat: 발주 조회 구현

--- src/main/java/com/example/wms/order/application/service/GetOrderService.java
+++ src/main/java/com/example/wms/order/application/service/GetOrderService.java
@@ -1,10 +1,18 @@
 package com.example.wms.order.application.service;
 
+import com.example.wms.infrastructure.pagination.util.PageableUtils;
+import com.example.wms.order.adapter.in.dto.OrderResponseDto;
 import com.example.wms.order.application.domain.Order;
 import com.example.wms.order.application.port.in.GetOrderUseCase;
 import com.example.wms.order.application.port.out.GetOrderPort;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +23,13 @@ public class GetOrderService implements GetOrderUseCase {
     @Override
     public Order getOrder(Long orderId) {
         return getOrderPort.getOrder(orderId);
+    }
+
+    @Override
+    public Page<OrderResponseDto> getFilteredOrder(String orderNumber, LocalDate startDate, LocalDate endDate, Pageable pageable) {
+        Pageable safePageable = PageableUtils.convertToSafePageableStrict(pageable, OrderResponseDto.class);
+        List<OrderResponseDto> orderList = getOrderPort.findOrderFilteringWithPageNation(orderNumber, startDate, endDate, safePageable);
+        Integer count = getOrderPort.countAllOrder(orderNumber, startDate, endDate);
+        return new PageImpl<>(orderList, pageable, count);
     }
 }
